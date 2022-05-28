70b75888c4f45d6c0896a8e0bcf68bdf96909c29
salia-etech <105612881+salia-etech@users.noreply.github.com>
2022-05-27 21:40:12 -0400

    Update install-script.sh

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -70,8 +70,8 @@ mvn -v
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
 sudo apt update && sudo apt upgrade
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update
 sudo apt install -y jenkins
 systemctl daemon-reload
