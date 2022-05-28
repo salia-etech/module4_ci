653c65dfa5812e5d1d9626812cfe9fd595d0ba23
salia-etech <105612881+salia-etech@users.noreply.github.com>
2022-05-27 21:32:07 -0400

    Update install-script.sh
    jenkins correct GPG key added

--- setup/vm-install-script/install-script.sh
+++ setup/vm-install-script/install-script.sh
@@ -69,7 +69,8 @@ mvn -v
 
 
 echo ".........----------------#################._.-.-JENKINS-.-._.#################----------------........."
-wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo apt update && sudo apt upgrade
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt update
 sudo apt install -y jenkins
